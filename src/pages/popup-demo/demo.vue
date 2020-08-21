fe1e33f23e2f9be374d754b4253a69cd25ef2883
YANGJIAKUN <200000725@163.com>
2020-08-21 12:13:46 +0800

    说明文档

--- src/pages/popup-demo/demo.vue
+++ src/pages/popup-demo/demo.vue
@@ -1,3 +1,84 @@
 <template>
-    <div>demo</div>
+    <page-slot title="popup弹出层">
+        <div class="card">
+            <h2>基本用法</h2>
+            <pre><code><span class="hljs-comment">&lt;!-- v-model绑定值来决定元素的显示和隐藏 --&gt;</span>
+    &lt;<span class="hljs-name">star-button</span> @<span class="hljs-attr">click</span>="<span class="hljs-string">showPopup = true</span>"&gt;打开弹窗&lt;/<span class="hljs-name">star-button</span>&gt;
+    &lt;<span class="hljs-name">star-popup</span> <span class="hljs-attr">v-model</span>="<span class="hljs-string">showPopup</span>" /&gt;
+    <span class="hljs-keyword">export default</span> {
+        data() {
+            <span class="hljs-attr">showPopup</span>: <span class="hljs-string">false</span>
+        }
+    }
+
+    <span class="hljs-comment">&lt;!-- 除此之外还通过一个模糊背景的属性，给弹出层添加filter属性即可 --&gt;</span>
+    &lt;<span class="hljs-name">star-button</span> @<span class="hljs-attr">click</span>="<span class="hljs-string">showPopup = true</span>"&gt;开启模糊背景&lt;/<span class="hljs-name">star-button</span>&gt;
+    &lt;<span class="hljs-name">star-popup</span> <span class="hljs-attr">v-model</span>="<span class="hljs-string">showPopup</span>" <span class="hljs-attr">filter</span> /&gt;
+
+    <span class="hljs-keyword">export default</span> {
+        data() {
+            <span class="hljs-attr">showPopup</span>: <span class="hljs-string">fasle</span>
+        }
+    }
+    </code></pre>
+        </div>
+        <div class="card">
+            <h2>弹出位置</h2>
+            <pre><code><span class="hljs-comment">&lt;!-- 通过定义position属性来设置弹出层出现的位置，可选值为：center、top、bottom、left、right --&gt;</span>
+    &lt;<span class="hljs-name">star-button</span> @<span class="hljs-attr">click</span>="<span class="hljs-string">showPopupPosition('center')</span>"&gt;居中弹出&lt;/<span class="hljs-name">star-button</span>&gt;
+    &lt;<span class="hljs-name">star-button</span> @<span class="hljs-attr">click</span>="<span class="hljs-string">showPopupPosition('top')</span>"&gt;顶部弹出&lt;/<span class="hljs-name">star-button</span>&gt;
+    &lt;<span class="hljs-name">star-button</span> @<span class="hljs-attr">click</span>="<span class="hljs-string">showPopupPosition('bottom')</span>"&gt;底部弹出&lt;/<span class="hljs-name">star-button</span>&gt;
+    &lt;<span class="hljs-name">star-button</span> @<span class="hljs-attr">click</span>="<span class="hljs-string">showPopupPosition('left')</span>"&gt;左侧弹出&lt;/<span class="hljs-name">star-button</span>&gt;
+    &lt;<span class="hljs-name">star-button</span> @<span class="hljs-attr">click</span>="<span class="hljs-string">showPopupPosition('right')</span>"&gt;右侧弹出&lt;/<span class="hljs-name">star-button</span>&gt;
+    &lt;<span class="hljs-name">star-popup</span> <span class="hljs-attr">v-model</span>="<span class="hljs-string">showPopup</span>" :<span class="hljs-attr">position</span>="<span class="hljs-string">position</span>" /&gt;
+
+    <span class="hljs-keyword">export default</span> {
+        data() {
+            <span class="hljs-attr">showPopup</span>: <span class="hljs-string">false</span>
+            <span class="hljs-attr">position</span>: "<span class="hljs-string"></span>"
+        },
+        methods: {
+            <span class="hljs-keyword">showPopupPosition</span>(position){
+                <span class="hljs-attr">this.position</span> = <span class="hljs-attr">position</span>
+                <span class="hljs-attr">this.showPopup</span> = <span class="hljs-attr">true</span>
+            }
+        }
+    }
+    </code></pre>
+        </div>
+        <!-- props -->
+        <props-table :propsData="propsData"></props-table>
+        <!-- events -->
+        <event-table :eventsData="eventsData"></event-table>
+    </page-slot>
 </template>
+<script>
+export default {
+    data() {
+        return {
+            propsData: [
+                {
+                    value: 'filter',
+                    decs: '是否需要背景模糊',
+                    type: 'boolean',
+                    default: 'false',
+                },
+                {
+                    value: 'position',
+                    decs:
+                        '弹出层内容弹出的位置，可选值为：center、top、bottom、left、right',
+                    type: 'string',
+                    default: 'center',
+                },
+            ],
+            eventsData: [
+                {
+                    name: 'close',
+                    decs: '关闭弹出层时触发',
+                    value: '——',
+                },
+            ],
+        }
+    },
+}
+</script>
--- src/route/index.js
+++ src/route/index.js
@@ -6,38 +6,26 @@ import home from '../pages/home.vue'
 import install from '../pages/install'
 // 颜色主题
 import theme from '../pages/theme'
-// 组件首页
-import comIndex from '../pages/com-index'
 // transition过渡
 import transitionText from '../pages/transition-demo/demo'
-import transitionCom from '../pages/transition-demo/transition'
 // 输入框
 import inputText from '../pages/input-demo/demo'
-import inputCom from '../pages/input-demo/input'
 // 按钮
 import buttonText from '../pages/button-demo/demo'
-import buttonCom from '../pages/button-demo/button'
 // 图片
 import imageText from '../pages/image-demo/demo'
-import imageCom from '../pages/image-demo/image'
 // 图标
 import iconText from '../pages/icon-demo/demo'
-import iconCom from '../pages/icon-demo/icon'
 // 复选框
 import checkboxText from '../pages/checkbox-demo/demo'
-import checkboxCom from '../pages/checkbox-demo/checkbox'
 // 导航
 import navbarText from '../pages/navbar-demo/demo'
-import navbarCom from '../pages/navbar-demo/navbar'
 // 分类栏
 import tabText from '../pages/tab-demo/demo'
-import tabCom from '../pages/tab-demo/tab'
 // 消息通知
 import noticeText from '../pages/notice-demo/demo'
-import noticeCom from '../pages/notice-demo/notice'
 // 弹出层
 import popupText from '../pages/popup-demo/demo'
-import popupCom from '../pages/popup-demo/popup'
 
 Vue.use(VueRouter)
 
@@ -57,73 +45,73 @@ const routes = [
                 path: '/home/install',
                 name: 'install',
                 meta: { name: 'star-ui' },
-                component: install
+                component: install,
             },
             {
                 path: '/home/theme',
                 name: 'theme',
                 meta: { name: 'star-ui' },
-                component: theme
+                component: theme,
             },
             {
                 path: '/home/transition',
                 name: 'transition-demo',
                 meta: { name: 'star-transition' },
-                component: transitionText
+                component: transitionText,
             },
             {
                 path: '/home/input',
                 name: 'input-demo',
                 meta: { name: 'star-input' },
-                component: inputText
+                component: inputText,
             },
             {
                 path: '/home/button',
                 name: 'button-demo',
                 meta: { name: 'star-button' },
-                component: buttonText
+                component: buttonText,
             },
             {
                 path: '/home/image',
                 name: 'image-demo',
                 meta: { name: 'star-image' },
-                component: imageText
+                component: imageText,
             },
             {
                 path: '/home/icon',
                 name: 'icon-demo',
                 meta: { name: 'star-icon' },
-                component: iconText
+                component: iconText,
             },
             {
                 path: '/home/checkbox',
                 name: 'checkbox-demo',
                 meta: { name: 'star-checkbox' },
-                component: checkboxText
+                component: checkboxText,
             },
             {
                 path: '/home/navbar',
                 name: 'navbar-demo',
                 meta: { name: 'star-navbar' },
-                component: navbarText
+                component: navbarText,
             },
             {
                 path: '/home/tabbar',
                 name: 'tabbar-demo',
                 meta: { name: 'star-tab' },
-                component: tabText
+                component: tabText,
             },
             {
                 path: '/home/notice',
                 name: 'notice-demo',
                 meta: { name: 'star-notice' },
-                component: noticeText
+                component: noticeText,
             },
             {
                 path: '/home/popup',
                 name: 'popup-demo',
                 meta: { name: 'star-popup' },
-                component: popupText
+                component: popupText,
             },
         ],
     },
